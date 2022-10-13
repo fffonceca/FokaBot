f18d1dd2af535e91ff2754c03a9b112dab7d6203
fffonceca <fffonseca@uc.cl>
2022-10-13 20:46:10 -0300

    ignore and PEP8

--- src/main.py
+++ src/main.py
@@ -3,9 +3,11 @@ import os
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
